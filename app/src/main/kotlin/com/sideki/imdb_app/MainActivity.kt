01b1da92aba49a92fec117a2e2ab6b048b702bf9
Eugene <eugene.zmnn@gmail.com>
2022-12-28 00:05:34 +0300

    TASK-11: Bottom nav view

--- app/src/main/kotlin/com/sideki/imdb_app/MainActivity.kt
+++ app/src/main/kotlin/com/sideki/imdb_app/MainActivity.kt
@@ -1,14 +1,35 @@
 package com.sideki.imdb_app
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentManager.BackStackEntry
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.sideki.imdb_app.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : FragmentActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentNavHost)
+        navController = navHostFragment!!.findNavController()
+        binding.bottomNavigation.setupWithNavController(navController)
+    }
+
+    override fun onMenuItemSelected(featureId: Int, item: MenuItem): Boolean {
+        NavigationUI.onNavDestinationSelected(item, navController)
+        return super.onMenuItemSelected(featureId, item)
     }
 }
