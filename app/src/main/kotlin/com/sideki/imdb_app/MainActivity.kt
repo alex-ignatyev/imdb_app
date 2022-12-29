a1c783f43b0899469e5f1ed99e2397134544c41a
Eugene <eugene.zmnn@gmail.com>
2022-12-29 23:05:42 +0300

    TASK-11: Bottom nav view

--- app/src/main/kotlin/com/sideki/imdb_app/MainActivity.kt
+++ app/src/main/kotlin/com/sideki/imdb_app/MainActivity.kt
@@ -2,6 +2,7 @@ package com.sideki.imdb_app
 
 import android.os.Bundle
 import android.view.MenuItem
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentManager.BackStackEntry
@@ -10,7 +11,9 @@ import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
+import com.sideki.imdb_app.data.api.ProfileFragment
 import com.sideki.imdb_app.databinding.ActivityMainBinding
+import com.sideki.imdb_app.ui.movies_list.MoviesFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -23,13 +26,20 @@ class MainActivity : FragmentActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        var fragment: Fragment = MoviesFragment()
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentNavHost)
         navController = navHostFragment!!.findNavController()
         binding.bottomNavigation.setupWithNavController(navController)
+        binding.bottomNavigation.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.moviesFragment -> bottomNavigation(R.id.moviesFragment, it)
+                R.id.profileFragment -> bottomNavigation(R.id.profileFragment, it)
+            }
+            return@setOnItemSelectedListener true
+        }
     }
-
-    override fun onMenuItemSelected(featureId: Int, item: MenuItem): Boolean {
+    private fun bottomNavigation(fragmentId: Int, item: MenuItem) {
         NavigationUI.onNavDestinationSelected(item, navController)
-        return super.onMenuItemSelected(featureId, item)
+        navController.navigate(fragmentId)
     }
 }
