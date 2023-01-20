1f7feffa1c608bcdc498c524825177b6f44f79a1
Eugene <eugene.zmnn@gmail.com>
2023-01-20 22:10:52 +0300

    TASK-15: верстка экрана авторизации + энтити и дао для аккаунта

--- app/src/main/kotlin/com/sideki/imdb_app/ui/login/LoginFragment.kt
+++ app/src/main/kotlin/com/sideki/imdb_app/ui/login/LoginFragment.kt
@@ -1,7 +1,22 @@
 package com.sideki.imdb_app.ui.login
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.sideki.imdb_app.R
+import com.sideki.imdb_app.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment(R.layout.fragment_login) {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val binding = FragmentLoginBinding.inflate(inflater, container, false)
+        binding.signUp.setOnClickListener {
+            val action = LoginFragmentDirections.actionLoginFragment2ToRegistrationFragment()
+            findNavController().navigate(action)
+        }
+        return binding.root
+    }
 }
--- app/src/main/kotlin/com/sideki/imdb_app/ui/registration/RegistrationFragment.kt
+++ app/src/main/kotlin/com/sideki/imdb_app/ui/registration/RegistrationFragment.kt
@@ -1,7 +1,30 @@
 package com.sideki.imdb_app.ui.registration
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.sideki.imdb_app.R
+import com.sideki.imdb_app.databinding.FragmentLoginBinding
+import com.sideki.imdb_app.databinding.FragmentRegistrationBinding
 
-class RegistrationFragment: Fragment(R.layout.fragment_registration) {
+class RegistrationFragment : Fragment(R.layout.fragment_registration) {
+    private lateinit var binding: FragmentRegistrationBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentRegistrationBinding.inflate(inflater, container, false)
+        passwordEqualityCheck()
+        return binding.root
+    }
+
+    private fun passwordEqualityCheck(){
+        if (binding.passwordInput.text != binding.repeatPasswordInput.text) {
+            binding.passwordField.helperText = "пароли не совпадают"
+            binding.repeatPasswordField.helperText = "пароли не совпадают"
+        } else {
+            binding.passwordField.helperText = ""
+            binding.repeatPasswordField.helperText = ""
+        }
+    }
 }
