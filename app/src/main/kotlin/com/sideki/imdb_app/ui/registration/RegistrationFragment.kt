d6cf5e587555a7cea954c9350056a3d68dc18887
Eugene <eugene.zmnn@gmail.com>
2023-01-28 08:19:33 +0300

    TASK-16: helpertext

--- app/src/main/kotlin/com/sideki/imdb_app/ui/registration/RegistrationFragment.kt
+++ app/src/main/kotlin/com/sideki/imdb_app/ui/registration/RegistrationFragment.kt
@@ -5,6 +5,7 @@ import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
 import androidx.core.widget.addTextChangedListener
+import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.sideki.imdb_app.R
@@ -18,31 +19,15 @@ class RegistrationFragment : Fragment(R.layout.fragment_registration) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val binding = FragmentRegistrationBinding.bind(view)
-        val loginInput = binding.loginInput.text.toString()
-        val passwordInput = binding.passwordInput.text.toString()
-        vm.loginError.observe(viewLifecycleOwner) {
-            binding.loginInput.addTextChangedListener(object : TextWatcher{
-                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-                    TODO("Not yet implemented")
-                }
-
-                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                    TODO("Not yet implemented")
-                }
-
-                override fun afterTextChanged(s: Editable?) {
-                    vm.loginError.observe(viewLifecycleOwner){
-                        binding.loginField.helperText = vm.loginValidation(s).toString()
-                    }
-                }
-
-            })
-            vm.passwordError.observe(viewLifecycleOwner) {
-                binding.passwordField.helperText = vm.passwordValidation(passwordInput).toString()
-            }
+        binding.loginInput.doAfterTextChanged {
+            binding.loginField.helperText = vm.loginValidation(it.toString())
+        }
+        binding.passwordInput.doAfterTextChanged {
+            binding.passwordField.helperText = vm.passwordValidation(it.toString())
 
         }
-
-
+        binding.repeatPasswordInput.doAfterTextChanged {
+            binding.repeatPasswordField.helperText = vm.passwordValidation(it.toString())
+        }
     }
 }
--- app/src/main/kotlin/com/sideki/imdb_app/ui/registration/RegistrationVM.kt
+++ app/src/main/kotlin/com/sideki/imdb_app/ui/registration/RegistrationVM.kt
@@ -1,6 +1,7 @@
 package com.sideki.imdb_app.ui.registration
 
 import android.text.Editable
+import android.view.View
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -12,13 +13,25 @@ class RegistrationVM @Inject constructor() : ViewModel() {
     val loginError = MutableLiveData<String>()
     val passwordError = MutableLiveData<String>()
 
-    fun loginValidation(input: Editable?) {
-        if (input!!.length < 8) loginError.value = "Minimum 8 Character Login"
+    fun loginValidation(input: String): String {
+        var helperText = "Required"
+        if (input.length < 8) helperText = "Minimum 8 Character Login"
+        if (input.isNotEmpty() && input.length >= 8) helperText = ""
+        return helperText
     }
 
-    fun passwordValidation(input: String) {
-        if (input.length < 8) passwordError.value = "Minimum 8 Character Password"
-        if (input.matches(".*[@#\$%^&+=].*".toRegex())) passwordError.value =
+    fun passwordValidation(input: String): String {
+        var helperText = "Required"
+        if (input.length < 8) helperText = "Minimum 8 Character Password"
+        if (input.length >= 8 && !input.matches(".*[@#\$%^&+=].*".toRegex())) helperText =
             "Must Contain 1 Special Character (@#\$%^&+=)"
+        if (input.length >= 8 && input.matches(".*[@#\$%^&+=].*".toRegex())) helperText = ""
+        return helperText
+    }
+
+    fun passwordEqualityCheck(input: View, input2: View): String {
+        var helperText = ""
+        if (input != input2) helperText = "Пароли не совпадают"
+        return helperText
     }
 }
